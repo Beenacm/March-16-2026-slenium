fa3762d2d3e007fc872d4de40fd539c0e2fed8c1
beena <cmbeena1@gmail.com>
2026-03-16 22:02:43 +0530

    added headless mode

--- src/test/java/TestComponents/BaseTest.java
+++ src/test/java/TestComponents/BaseTest.java
@@ -9,11 +9,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Properties;
 
+import javax.swing.tree.AbstractLayoutCache.NodeDimensions;
+
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -32,16 +36,23 @@ public class BaseTest {
 		FileInputStream fr = new FileInputStream(
 				System.getProperty("user.dir") + "\\src\\main\\java\\resources\\GlobalData.properties");
 		prop.load(fr);
-		String browserName =System.getProperty("browser")!=null?System.getProperty("browser"):prop.getProperty("browser");
-		
-		 //prop.getProperty("browser");
+		String browserName = System.getProperty("browser") != null ? System.getProperty("browser")
+				: prop.getProperty("browser");
 
-		if (browserName.equalsIgnoreCase("chrome")) {
+		// prop.getProperty("browser");
 
-			driver = new ChromeDriver();
+		if (browserName.contains("chrome")) {
+
+			ChromeOptions options = new ChromeOptions();
+
+			if (browserName.contains("headless")) {
+				options.addArguments("headless");
+			}
+			driver = new ChromeDriver(options);
+			driver.manage().window().setSize(new Dimension(1440,900));
 
 		} else if (browserName.equalsIgnoreCase("Firefox")) {
-			driver=new FirefoxDriver();
+			driver = new FirefoxDriver();
 		}
 
 		driver.manage().window().maximize();
@@ -50,7 +61,7 @@ public class BaseTest {
 
 	}
 
-	@BeforeMethod(alwaysRun=true)
+	@BeforeMethod(alwaysRun = true)
 	public LoginPage launchApplication() throws IOException {
 		driver = initializeDriver();
 		loginPage = new LoginPage(driver);
@@ -58,7 +69,7 @@ public class BaseTest {
 		return loginPage;
 	}
 
-	@AfterMethod(alwaysRun=true)
+	@AfterMethod(alwaysRun = true)
 	public void tearDown() {
 		driver.close();
 	}
@@ -77,13 +88,12 @@ public class BaseTest {
 
 	}
 
-	public String getScreenShot(String testCaseName,WebDriver driver) throws IOException {
-		TakesScreenshot ts = (TakesScreenshot)driver;
-		File file=ts.getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(file, new File(System.getProperty("user.dir")+"\\reports\\"+testCaseName+".png"));
-		return System.getProperty("user.dir")+"\\reports\\"+testCaseName+".png";
-		
-		
+	public String getScreenShot(String testCaseName, WebDriver driver) throws IOException {
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File file = ts.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(file, new File(System.getProperty("user.dir") + "\\reports\\" + testCaseName + ".png"));
+		return System.getProperty("user.dir") + "\\reports\\" + testCaseName + ".png";
+
 	}
 
 }
